1e705d1ac464084765c3dba1301050df301976e8
hanan.kibria-at-101380220521 <hanan.kibria@gosaas.io>
2022-03-28 19:41:43 +0500

    /

--- UI/secretserver/src/config.js
+++ UI/secretserver/src/config.js
@@ -1,6 +1,6 @@
 const Url = {
     // production: 'http://demo-dataportal.gosaas.io:5000',
-    production: 'https://secretserver1234.herokuapp.com/',
+    production: 'https://secretserver1234.herokuapp.com',
     //production:'https://coast-dev-portal.gosaas.io',
     development: 'http://localhost:5000',
     //production:"http://localhost:5000"
